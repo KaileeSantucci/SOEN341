bd5cc034774c8e9cff71574b7075c57cefc43cb7
Riad-Rakan <rakanrriad@gmail.com>
2025-03-02 14:46:14 -0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 
+import { onAuthStateChanged } from "firebase/auth";
+
 const App = () => {
+    const user = false;
+
+    useEffect(()=>{
+      const unSub = onAuthStateChanged(auth,(user)=>{
+        console.log(user);
+      });
+
+      return () => {
+        unSub();
+      };
+    },[]);
+
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+      {user ? (
+        <>
+          <List/>
+          <Chat/>
+          <Detail/>
+        </>
+      ) : (
+        <Login/>
+      )}
+
     </div>
   )
 }
