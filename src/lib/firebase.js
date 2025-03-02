855fff050fc7e59052fe81629bffca45f932dd2c
Riad-Rakan <rakanrriad@gmail.com>
2025-03-02 07:08:34 -0500

    firebase data and link created

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/anuth";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "soen341-classproject.firebaseapp.com",
+  databaseURL: "https://soen341-classproject-default-rtdb.firebaseio.com",
+  projectId: "soen341-classproject",
+  storageBucket: "soen341-classproject.firebasestorage.app",
+  messagingSenderId: "843482660549",
+  appId: "1:843482660549:web:c40edd4f47e16737b5baac",
+  measurementId: "G-K2E8TF59GL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+export const db = getFirestore();
