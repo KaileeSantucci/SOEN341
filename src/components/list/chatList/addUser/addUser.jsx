be2606cabf7f67b542232af96b621a6c1e057cbe
Riad-Rakan <rakanrriad@gmail.com>
2025-03-02 06:22:49 -0500

    Created and modified new components for searching and adding/following users through the chat list created previously

---
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -0,0 +1,22 @@
+import "./addUser"
+
+const addUser = () =>{
+    return (
+        <div className="addUser">
+            <form>
+                <input type="text" placeholder="Username" name="username"/>
+                <button>Search</button>
+            </form>
+            <div className="user">
+                <div className="detail">
+                    <img src="./avatar.png" alt="" />
+                    <span>Medium Cheese</span>
+                </div>
+                <button>Add User</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default AddUser;
