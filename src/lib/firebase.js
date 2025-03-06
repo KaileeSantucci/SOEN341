b5a89bf0ccde61c4effa35db8fd33bae7d6b71c8
Riad-Rakan <rakanrriad@gmail.com>
2025-03-06 15:37:38 -0500

    enountering auth not defined error

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -19,5 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth();
-export const db = getFirestore();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
