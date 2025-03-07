39f6cc06aa9351d4a987c46acbc03f8c5baa3fc9
Riad-Rakan <rakanrriad@gmail.com>
2025-03-07 08:22:03 -0500

    firbase.js file errror

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import Login from "./components/login/login";
 import Notification from "./components/notification/notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./components/lib/userStore";
 
 const App = () => {
 
-  const {currentUser, isLoading, fetchUserInfo} = useUserStore()
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
--- src/components/lib/firebase.js
+++ src/components/lib/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: VIRE_API_KEY,
-  authDomain: "soen341-classproject.firebaseapp.com",
-  databaseURL: "https://soen341-classproject-default-rtdb.firebaseio.com",
-  projectId: "soen341-classproject",
-  storageBucket: "soen341-classproject.firebasestorage.app",
-  messagingSenderId: "843482660549",
-  appId: "1:843482660549:web:c40edd4f47e16737b5baac",
-  measurementId: "G-K2E8TF59GL"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchat-8953e.firebaseapp.com",
+  projectId: "reactchat-8953e",
+  storageBucket: "reactchat-8953e.appspot.com",
+  messagingSenderId: "989490756392",
+  appId: "1:989490756392:web:9e54e92c7b39c531b82e1d"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth()
+export const db = getFirestore()
